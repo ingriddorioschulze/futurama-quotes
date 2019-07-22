fda89e53a0a11eb16cb51498a5a30c7bf505eb93
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2019-07-22 11:18:08 +0200

    handling state with Redux

--- src/actions.js
+++ src/actions.js
@@ -22,9 +22,14 @@ export function closeSearchResults() {
 }
 
 export function quotes(quotes) {
-  return axios.get(
-    `https://futuramaapi.herokuapp.com/api/characters/${quotes.name}`
-  );
+  return axios
+    .get(`https://futuramaapi.herokuapp.com/api/characters/${quotes.name}`)
+    .then(response => {
+      return {
+        type: "QUOTES_DATA",
+        data: response.data
+      };
+    });
 }
 
 // import { CHANGE_SEARCH_FIELD } from "./constants.js";
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import App from "./containers/App";
+import { searchCharacters } from "./reducer";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const logger = createLogger();
+const store = createStore(searchCharacters, applyMiddleware(logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
